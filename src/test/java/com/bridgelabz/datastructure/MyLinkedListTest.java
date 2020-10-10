385862e04622a5ce478b507b333e260c62cf3b44
Kauk777 <curokky777@gmail.com>
2020-10-10 15:25:29 +0530

    [KAUSHAL]Added: UC4 inserting between nodes

--- src/test/java/com/bridgelabz/datastructure/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/datastructure/MyLinkedListTest.java
@@ -20,7 +20,7 @@ public class MyLinkedListTest {
 		Assert.assertTrue(result);
 	}
 	@Test
-	public void given3NumbersWhenAppendedToLinkedListShouldBeAddedToTop() {
+	public void given3NumbersWhenAppendedToLinkedListShouldPassLinkedListResult() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
@@ -29,6 +29,20 @@ public class MyLinkedListTest {
 		myLinkedList.append(myFirstNode);
 		myLinkedList.append(mySecondNode);
 		myLinkedList.append(myThirdNode);
+		myLinkedList.printMyNodes();
+		boolean result=myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+	@Test
+	public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListShouldResult() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		
+		MyLinkedList myLinkedList=new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.insert(myFirstNode,mySecondNode);
 		myLinkedList.printMyNodes();
 		boolean result=myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
