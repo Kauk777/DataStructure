1347f06620ef09a38f4ccb8f3a6f8906fd371920
Kauk777 <curokky777@gmail.com>
2020-10-12 22:49:22 +0530

    [KAUSHAL]Added: BSTUC1 creating BST of size 3

---
+++ src/test/java/com/bridgelabz/datastructure/MyBinaryTreeTest.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.datastructure;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MyBinaryTreeTest {
+
+	@Test
+	public void given3NumbersWhenAddedToBSTShouldReturnSize3() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(3, size);
+	}
+
+}
