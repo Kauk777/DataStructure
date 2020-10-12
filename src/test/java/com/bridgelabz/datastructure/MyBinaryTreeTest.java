87f3de92970479592a4ad417b5ee0c43a7a7fb8d
Kauk777 <curokky777@gmail.com>
2020-10-12 23:09:01 +0530

    [KAUSHAL]Added: BSTUC2 creating given BST

--- src/test/java/com/bridgelabz/datastructure/MyBinaryTreeTest.java
+++ src/test/java/com/bridgelabz/datastructure/MyBinaryTreeTest.java
@@ -14,5 +14,15 @@ public class MyBinaryTreeTest {
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(3, size);
 	}
+	
+	@Test
+	public void givenNumbersWhenAddedToBSTShouldReturnSize() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		int arr[]= {56,30,70,22,40,60,95,65,63,67,11,3,16};
+		for(Integer i:arr)
+			myBinaryTree.add(i);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
+	}
 
 }
